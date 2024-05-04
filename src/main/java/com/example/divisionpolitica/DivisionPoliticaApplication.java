03a0ec5f463de7686fe45c4538c9ecb47e5a2863
sebastian <sebazgrisales@gmail.com>
2024-05-03 22:01:38 -0500

    first commit

---
+++ src/main/java/com/example/divisionpolitica/DivisionPoliticaApplication.java
@@ -0,0 +1,13 @@
+package com.example.divisionpolitica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivisionPoliticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DivisionPoliticaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/divisionpolitica/DivisionPoliticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.divisionpolitica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivisionPoliticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
